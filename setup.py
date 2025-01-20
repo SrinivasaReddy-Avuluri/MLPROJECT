2516af709ee3e656251a13768f65e9ee19f2a705
SrinivasaReddy-Avuluri <srinivasareddyavuluri14@gmail.com>
2025-01-20 10:20:34 +0530

    adding setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='MLPROJECT',
+    version='0.0.1',
+    author='SrinivasaReddy-Avuluri',
+    author_email='srinivasareddyavuluri14@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn','matplotlib']
+)
