db5a1e7b4542a64eeff628b2d99c9a719d53f173
SrinivasaReddy-Avuluri <srinivasareddyavuluri14@gmail.com>
2025-01-22 13:38:46 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requires(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+        return requirements
 
 setup(
     name='MLPROJECT',
@@ -6,5 +19,5 @@ setup(
     author='SrinivasaReddy-Avuluri',
     author_email='srinivasareddyavuluri14@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn','matplotlib']
+    install_requires=get_requires('requirements.txt')
 )
